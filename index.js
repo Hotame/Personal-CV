34ca61d1809b9f9dec1093f709f012e5aeea1829
Timmy <hatem.bassah@gmail.com>
2024-02-23 13:01:45 +0300

    New Changes

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 import express from 'express';
+import cors from 'cors';
 import nodemailer from 'nodemailer';
 import bodyParser from 'body-parser';
 import { dirname, join } from 'path';
@@ -9,13 +10,14 @@ const app = express();
 const port = process.env.PORT || 3000; // Use the provided PORT or default to 3000
 
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cors());
 app.use(express.static(join(__dirname, 'public')));
 
 app.get('/', (req, res) => {
     res.sendFile(join(__dirname, 'public', 'index.html'));
 });
 
-app.post('/', (req, res) => {
+app.post('/submit', (req, res) => {
     const { email, message } = req.body;
     console.log(req.body);
 
