0164bfc22c44bbc9ad5d5f6fb96aa4735793774a
Timmy <hatem.bassah@gmail.com>
2024-02-23 10:57:45 +0300

    Code and UI Changes

---
+++ index.js
@@ -0,0 +1,48 @@
+import express from 'express';
+import nodemailer from 'nodemailer';
+import bodyParser from 'body-parser';
+import { dirname } from "path";
+import { fileURLToPath } from "url";
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
+const app = express();
+const port = 3000;
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static(__dirname + '/public'));
+app.use('/images', express.static(__dirname + '/images'));
+
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/index.html');
+});
+
+app.post('/submit', (req, res) => {
+    const {email, message } = req.body;
+    console.log(req.body);
+
+    const transporter = nodemailer.createTransport({
+        service: 'gmail',
+        auth: {
+            user: 'hatem.bassah@gmail.com',
+            pass: 'crew csbo pkqb igmc'
+        }
+    });
+
+    const mail = {
+        from: email,
+        to: 'hatem.bassah@gmail.com',
+        subject: '',
+        text: `${message}`
+    };
+
+    transporter.sendMail(mail, (error, info) => {
+        if (error) {
+            return console.error(error);
+        }
+        res.redirect('/');
+    });
+});
+
+app.listen(port, () => {
+    console.log(`Server is running on http://localhost:${port}`);
+});
--- script.js
+++
@@ -1,7 +0,0 @@
-function showMenu() {
-    document.getElementById("links").style.right = "0";
-}
-
-function hideMenu() {
-    document.getElementById("links").style.right = "-100%";
-}
